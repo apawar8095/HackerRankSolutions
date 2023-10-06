45baf22523d005d20d99f54a8c39903eb7a53fe1
apawar8095 <106057075+apawar8095@users.noreply.github.com>
2023-10-06 15:06:07 +0530

    Readme
    Python : missing Character
    
    sample input 
    8hypotheticall024y6wxz
    
    sample output 
    13579bdfgjkmnqrsuv

---
+++ MissingCharacter.py
@@ -0,0 +1,47 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'missingCharacters' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def missingCharacters(s):
+    # Initialize lists to store missing digits and characters
+    missing_digits = []
+    missing_chars = []
+
+    # Iterate through lowercase letters and digits
+    for char in '0123456789':
+        if char not in s:
+            missing_digits.append(char)
+
+    for char in 'abcdefghijklmnopqrstuvwxyz':
+        if char not in s:
+            missing_chars.append(char)
+
+    # Sort the missing digits and characters
+    missing_digits.sort()
+    missing_chars.sort()
+
+    # Combine and return the result with spaces after each character
+    result = ''.join([char + '' for char in missing_digits + missing_chars])
+    return result.rstrip()  # Remove the trailing space
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = missingCharacters(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
