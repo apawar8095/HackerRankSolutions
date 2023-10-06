c8a6a431d8cbeffeb92c552316f2205debd40098
apawar8095 <106057075+apawar8095@users.noreply.github.com>
2023-10-06 15:10:10 +0530

    Update StringRepresentationsofObjects.py

--- StringRepresentationsofObjects.py
+++ StringRepresentationsofObjects.py
@@ -7,8 +7,6 @@ import random
 import re
 import sys
 
-
-
 class Car:
     def __init__(self ,speed ,unit):
         self.spee d =speed
@@ -24,20 +22,20 @@ class Boat:
         str 1 ="Boat with the maximum speed of {} knots"
         return str1.format(self.speed)
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-    q = int(raw_input())
-    queries = []
-    for _ in xrange(q):
-        args = raw_input().split()
-        vehicle_type, params = args[0], args[1:]
-        if vehicle_type == "car":
-            max_speed, speed_unit = int(params[0]), params[1]
-            vehicle = Car(max_speed, speed_unit)
-        elif vehicle_type == "boat":
-            max_speed = int(params[0])
-            vehicle = Boat(max_speed)
-        else:
-            raise ValueError("invalid vehicle type")
-        fptr.write("%s\n" % vehicle)
-    fptr.close()
+# if __name__ == '__main__':
+#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+#     q = int(raw_input())
+#     queries = []
+#     for _ in xrange(q):
+#         args = raw_input().split()
+#         vehicle_type, params = args[0], args[1:]
+#         if vehicle_type == "car":
+#             max_speed, speed_unit = int(params[0]), params[1]
+#             vehicle = Car(max_speed, speed_unit)
+#         elif vehicle_type == "boat":
+#             max_speed = int(params[0])
+#             vehicle = Boat(max_speed)
+#         else:
+#             raise ValueError("invalid vehicle type")
+#         fptr.write("%s\n" % vehicle)
+#     fptr.close()
