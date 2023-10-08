20c4d573c23446f7271ae7a24fc7b56e0426fd3f
apawar8095 <106057075+apawar8095@users.noreply.github.com>
2023-10-08 14:09:29 +0530

    Add files via upload
    Palindrome Index 
    
    Sample Input
    
    STDIN   Function
    
    3	q=3
    aaab	s ='aaab'  1st query
    baa	s= 'baa'   2nd
    aaa	s = 'aaa'  third query
    
    sample o/p
    3
    0
    -1

---
+++ Palindrome_Index.java
@@ -0,0 +1,94 @@
+// Palindrome Index 
+
+// Sample Input
+
+// STDIN   Function
+// ----	------
+// 3	q=3
+// aaab	s ='aaab'  1st query
+// baa	s= 'baa'   2nd
+// aaa	s = 'aaa'  third query
+
+// sample o/p
+// 3
+// 0
+// -1
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+class Result {
+
+    /*
+     * Complete the 'palindromeIndex' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int palindromeIndex(String s) {
+    // Write your code here
+
+int length = s.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (s.charAt(i) != s.charAt(length - 1 - i)) {
+                if (isPalindrome(s, i + 1, length - 1 - i)) {
+                    return i;
+                } else if (isPalindrome(s, i, length - 2 - i)) {
+                    return length - 1 - i;
+                } else {
+                    return -1;
+                }
+            }
+        }
+        return -1;
+    }
+    private static boolean isPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    
+
+}
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = Result.palindromeIndex(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
