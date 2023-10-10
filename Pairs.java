5201a820787e729704d40016528ea42058eb80cf
apawar8095 <106057075+apawar8095@users.noreply.github.com>
2023-10-10 19:33:35 +0530

    Add files via upload
    Sample Input
    
    STDIN
    
    Function
    
    5 2
    
    arr[] size n = 5, k =2
    
    1 5 3 4 2
    
    r = [1, 5, 3, 4, 2]
    
    Sample Output
    
    3

---
+++ Pairs.java
@@ -0,0 +1,70 @@
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+class Result {
+
+    /*
+     * Complete the 'pairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static int pairs(int k, List<Integer> arr) {
+    // Write your code here
+    Set<Integer> set = new HashSet<>(arr);
+        int count = 0;
+
+        for (int num : arr) {
+            if (set.contains(num + k)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    }
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.pairs(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
